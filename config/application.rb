b7528d5500d83afa860cf421dd089749b4b8ab8e
gregory blain <gregory.blain@gmail.com>
2017-11-27 15:56:12 +0100

    Ajout modèles Buyer et Seller & changement du nom

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Rbnb
+module BuildNGreen
   class Application < Rails::Application
     config.generators do |generate|
       generate.assets false
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  devise_for :sellers
+  devise_for :buyers
+  root to: 'product#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
