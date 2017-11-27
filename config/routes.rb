1fdb19e26d8794b79bced79d3d7c44499edcec61
gregory blain <gregory.blain@gmail.com>
2017-11-27 17:49:15 +0100

    modif routes Seller et navbar personnalisée seller / buyer

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :sellers
 
   devise_for :sellers
   devise_for :buyers
+
+  resources :sellers
+
   root to: 'product#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
