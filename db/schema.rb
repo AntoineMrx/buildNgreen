30bdf0b72eea073cdcf842ac94aa81b73a059d70
gregory blain <gregory.blain@gmail.com>
2017-11-27 16:51:59 +0100

    ajout de nom prenom dans Seller et ajout des validations dans le modèle

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127153702) do
+ActiveRecord::Schema.define(version: 20171127154717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20171127153702) do
     t.integer  "delivery_time"
     t.integer  "delivery_fee"
     t.integer  "delivery_free_from"
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_sellers_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_sellers_on_reset_password_token", unique: true, using: :btree
   end
