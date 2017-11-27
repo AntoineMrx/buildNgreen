2676c3d8712df17bd011fc1800d77a42518eb3ac
Marc Thiry <marc.thiry@edhec.com>
2017-11-27 18:14:56 +0100

    path redirect for sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_up_path_for(resource)
+    if resource.is_a?(seller)
+      edit_buyer_registration_path
+    elsif resource.is_a?(buyer)
+      edit_buyer_registration_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :sellers
-  devise_for :buyers
+  devise_for :buyers, controllers: { registrations: "registrations" }
+
+  resources :buyers
+
   root to: 'product#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
