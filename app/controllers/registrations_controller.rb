1b35ebabc727ecdf77ba8f22df0431354fc6a1f3
Marc Thiry <marc.thiry@edhec.com>
2017-11-28 10:18:47 +0100

    morning fix du sign-up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   def after_sign_up_path_for(resource)
     if resource.is_a?(Seller)
-      edit_seller_path
+      edit_seller_path(resource)
     elsif resource.is_a?(Buyer)
-      edit_buyer_path
+      edit_buyer_path(resource)
     end
   end
 
