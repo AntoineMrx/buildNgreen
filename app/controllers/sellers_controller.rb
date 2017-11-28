1119e63e5b746447796574239ad2679ea9b879b2
Laetitia Ahamada <alaetitia@live.fr>
2017-11-28 13:36:29 +0100

    fait seller edit html et seller controller update

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,13 +1,26 @@
 class SellersController < ApplicationController
-  def index
-  end
 
-  def new
+  before_action :set_seller, only:[:edit, :update]
+  def index
   end
 
   def edit
   end
 
+  def update
+    @seller = seller.update(seller_params)
+  end
+
   def show
+  end
+
+   private
+
+  def seller_params
+    params.require(:seller).permit(:first_name, :last_name, :email, :address, :password, :description, :website, :company_name)
+  end
+
+  def set_seller
+    @seller = Seller.find(params[:id])
   end
 end
