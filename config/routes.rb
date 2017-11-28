509477413f3a0f2ebc33aa0fd444b05793b7dd4b
Antoine Marmoux <antoine.marmoux@hotmail.fr>
2017-11-28 18:44:43 +0100

    nesting order_new

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
   devise_for :sellers, controllers: { registrations: "registrations" }
   devise_for :buyers, controllers: { registrations: "registrations" }
 
-  resources :buyers
+  resources :buyers do
+    resources :orders, only: [:new]
+  end
+
   resources :sellers
   resources :products
   resources :categories
-  resources :orders
+  resources :orders, only: [:index, :show, :edit]
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
