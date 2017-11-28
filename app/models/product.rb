d25f990b8b54d8167c4a4fa03d7f45bedb1cddd2
Antoine Marmoux <antoine.marmoux@hotmail.fr>
2017-11-28 11:17:09 +0100

    products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :seller
   belongs_to :category
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :selling_price, presence: true
+  validates :retail_price, presence: true
+  validates :category, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   resources :buyers
   resources :sellers
+  resources :products
 
-  root to: 'product#index'
+  root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
