edae25521813e1d25d43ebf010d62ef450c95f86
Gregory Blain <gregory.blain@gmail.com>
2017-11-30 15:04:23 +0100

    Gestion panier et mention dans la navbar et icone clickable

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,5 +1,6 @@
 class BasketsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :add_item
+  before_action :load_basket, only: [:destroy, :edit]
 
   def show
 
@@ -7,26 +8,24 @@ class BasketsController < ApplicationController
 
   def index
     @array = get_items_from_cookie
+    @order = current_buyer.orders.where(status: "initiée")[0] if buyer_signed_in?
   end
 
   def add_item
     product_id = params[:id]
     if buyer_signed_in?
-      print "je suis dans le buyer"
-      id = current_buyer.id
-      order = order_en_cours(id)
+      order = order_en_cours
+      clean_cookies(order)
       if item_in_basket?(order, product_id)
         iterate_item(order, product_id)
       else
-        Basket.new(order_id: order.id, product_id: product_id, quantity: 1)
+        Basket.create(order_id: order.id, product_id: product_id, quantity: 1)
       end
-      print order.baskets
     elsif session[:basket].nil?
       session[:basket] = {}
       session[:basket][product_id.to_sym] = 1
     else
       basket = session[:basket]
-      puts "on est rentrés dans le else"
       if basket[product_id].nil?
         basket[product_id] = 1
       else
@@ -34,36 +33,46 @@ class BasketsController < ApplicationController
       end
     session[:basket] = basket
     end
-    print session[:basket]
     session[:basket]
+  end
+
+  def destroy
+    @basket.destroy
+    redirect_to baskets_path
   end
 
   private
 
-  def order_en_cours(buyer_id)
-    buyer = Buyer.find(buyer_id)
-    if buyer.orders.where(status:"initiée").nil?
-      order = Order.new(buyer_id: id, status:"initiée")
+  def load_basket
+    @basket = Basket.find(params[:id])
+  end
+
+  def order_en_cours
+    if current_buyer.orders.where(status:"initiée") == []
+      order = Order.new(buyer_id: current_buyer.id, status:"initiée")
       order.save
       return order
     else
-      puts "je vais printer l'order"
-      order = buyer.orders.where(status:"initiée")
-      puts order.class
-      puts "tu l'as vu ?"
+      order = current_buyer.orders.where(status:"initiée")[0]
       return order
     end
   end
 
+  def clean_cookies(order)
+    array = get_items_from_cookie
+    array.each do |basket|
+      basket.order_id = order.id
+      basket.save
+    end
+    session[:basket] = nil
+  end
+
   def item_in_basket?(order, product_id)
-    puts "voici #{order}"
-    puts order.class
-    puts order.baskets
-    !order.baskets.where(product_id: product_id).nil?
+    order.baskets.where(product_id: product_id).size > 0
   end
 
   def iterate_item(order, product_id)
-    basket = order.baskets.where(product_id: product_id)
+    basket = order.baskets.where(product_id: product_id)[0]
     basket.quantity += 1
   end
 
--- app/models/basket.rb
+++ app/models/basket.rb
@@ -2,4 +2,14 @@ class Basket < ApplicationRecord
   belongs_to :order
   belongs_to :product
   has_one :seller, through: :product
+
+  def self.items(buyer_id)
+    order = Buyer.find(buyer_id).orders.where(status:"initiée")[0]
+    nb = order.baskets.size
+    if nb == 0
+      return ""
+    else
+      return "(#{nb})"
+    end
+  end
 end
