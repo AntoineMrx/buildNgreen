f586fc08d2432d8edcfa9fa8b500288cb89cc7ac
Gregory Blain <gregory.blain@gmail.com>
2017-12-01 09:43:45 +0100

    navbar fix, logo vert, et h1 de la home blanc

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,6 @@ Rails.application.routes.draw do
   get "search", to: "products#search"
 
   root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
